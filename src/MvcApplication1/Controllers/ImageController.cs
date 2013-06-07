e61aaae21bbc15dcebb1cbf60664932738af53c3
Neils Christoffersen <neils.christoffersen@gmail.com>
2013-06-07 15:15:05 -0500

    MVC client example

---
+++ src/MvcApplication1/Controllers/ImageController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+using MvcApplication1.ImageService;
+
+namespace MvcApplication1.Controllers
+{
+    public class ImageController : Controller
+    {             
+        public async Task<ActionResult> Details(int id)
+        {
+            using (var serviceClient = new ImageServiceClient())
+            {
+                var base64ImageString = await serviceClient.DownloadImageAsync(id);
+
+                var bytes = Convert.FromBase64String(base64ImageString);
+                return File(bytes, "image/jpeg");
+            }
+        }
+    }
+}
